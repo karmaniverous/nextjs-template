ee4582cba6aba273358bf68a355f0e5b49c3749a
Jason Williscroft <me@karmanivero.us>
2023-02-10 00:06:51 +0800

    empty object

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 import { withAuth } from 'next-auth/middleware';
 
-export const middleware = withAuth();
+export const middleware = withAuth({});
 
 export const config = {
   matcher: ['/private'],
