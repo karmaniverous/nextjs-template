15f45e81651f82e42c31823c26187aa7c5495e77
Jason Williscroft <me@karmanivero.us>
2023-02-09 19:43:31 +0800

    added logging to middleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
-export { withAuth as middleware } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+
+export const middleware = withAuth((req) => {
+  console.log(req);
+});
 
 export const config = {
   matcher: ['/private'],
