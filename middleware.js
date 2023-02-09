b1223aa9a41c69488315f9c43c2442e992daef1b
Jason Williscroft <me@karmanivero.us>
2023-02-09 23:32:56 +0800

    more logging

--- middleware.js
+++ middleware.js
@@ -1,8 +1,18 @@
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth(async (req /*, res*/) => {
-  console.log(req);
-});
+export default withAuth(
+  async (req /*, res*/) => {
+    console.log({ function: 'withAuth', req });
+  },
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        console.log({ function: 'authorized', req, token });
+        return true;
+      },
+    },
+  }
+);
 
 export const config = {
   matcher: ['/private'],
