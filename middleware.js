b6982c670fac34aa8fdbe855c00ef3bca998b5c4
Jason Williscroft <me@karmanivero.us>
2023-02-09 23:11:28 +0800

    another middleware test

--- middleware.js
+++ middleware.js
@@ -1,12 +1,8 @@
-// import { withAuth } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
 
-// export const middleware = withAuth((req) => {
-//   console.log(req);
-// });
-
-export const middleware = (req) => {
+export default withAuth(async (req /*, res*/) => {
   console.log(req);
-};
+});
 
 export const config = {
   matcher: ['/private'],
