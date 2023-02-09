1cc3ea922c6394b7210040d6454ff16aed7f8da8
Jason Williscroft <me@karmanivero.us>
2023-02-09 23:44:27 +0800

    no middleware args

--- middleware.js
+++ middleware.js
@@ -1,18 +1,6 @@
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth(
-  async (req /*, res*/) => {
-    console.log({ function: 'withAuth', req });
-  },
-  {
-    callbacks: {
-      authorized: ({ req, token }) => {
-        console.log({ function: 'authorized', req, token });
-        return true;
-      },
-    },
-  }
-);
+export default withAuth({});
 
 export const config = {
   matcher: ['/private'],
